d37736b225a57372e76c7f298ba39b6e53381541
James Munns <james@onevariable.com>
2023-01-21 06:23:43 +0100

    Cleaner popslide

--- src/lib.rs
+++ src/lib.rs
@@ -82,8 +82,8 @@ enum Instruction {
     Z_IsWordHasChildrenIsLast,
 }
 
-impl Into<u8> for Instruction {
-    fn into(self) -> u8 {
+impl Instruction {
+    const fn to_u8(&self) -> u8 {
         match self {
             Instruction::S_NotWordNoChildrenNotLast => 0b00000_000,
             Instruction::T_NotWordNoChildrenIsLast => 0b00000_001,
@@ -94,6 +94,12 @@ impl Into<u8> for Instruction {
             Instruction::Y_IsWordHasChildrenNotLast => 0b00000_110,
             Instruction::Z_IsWordHasChildrenIsLast => 0b00000_111,
         }
+    }
+}
+
+impl Into<u8> for Instruction {
+    fn into(self) -> u8 {
+        self.to_u8()
     }
 }
 
@@ -124,9 +130,9 @@ impl Instr {
     const LEN_MASK: u8 = 0b11111_000;
     const LEN_SHIFT: u8 = 3;
 
-    fn from_len_instr(len: u8, instr: Instruction) -> Self {
+    const fn from_len_instr(len: u8, instr: Instruction) -> Self {
         assert!(len <= 0b000_11111);
-        let instr: u8 = instr.into();
+        let instr: u8 = instr.to_u8();
         let instr = instr & Self::INSTR_MASK;
         let slen = (len << Self::LEN_SHIFT) & Self::LEN_MASK;
 
@@ -172,6 +178,20 @@ impl Instr {
     fn len_u8(&self) -> u8 {
         (self.0 & Self::LEN_MASK) >> Self::LEN_SHIFT
     }
+
+    fn has_children(&self) -> bool {
+        // TODO: Could do with bitops?
+        match self.instr() {
+            Instruction::S_NotWordNoChildrenNotLast => false,
+            Instruction::T_NotWordNoChildrenIsLast => false,
+            Instruction::U_NotWordHasChildrenNotLast => true,
+            Instruction::V_NotWordHasChildrenIsLast => true,
+            Instruction::W_IsWordNoChildrenNotLast => false,
+            Instruction::X_IsWordNoChildrenIsLast => false,
+            Instruction::Y_IsWordHasChildrenNotLast => true,
+            Instruction::Z_IsWordHasChildrenIsLast => true,
+        }
+    }
 }
 
 struct T9Vm {
@@ -190,87 +210,78 @@ impl T9Vm {
         self.program_ctr = 0;
     }
 
+    fn pop_cstack(&mut self) -> Instr {
+        let val = self.control_stack.pop().unwrap();
+        self.word_stack.drop_n(val.len()).unwrap();
+        if val.is_word() {
+            self.prio_addr_stack.pop().unwrap();
+        }
+        val
+    }
+
     fn next_word(&mut self) -> Option<&str> {
         println!("+=+= NEXT WORD =+=+");
-        // Clear off any "last" positions
-        match self.control_stack.peek() {
-            Some(i) => {
-                match i.instr() {
-                    Instruction::S_NotWordNoChildrenNotLast => unreachable!(),
-                    Instruction::T_NotWordNoChildrenIsLast => unreachable!(),
-                    Instruction::U_NotWordHasChildrenNotLast => panic!("We should never leave off on !word?"),
-                    Instruction::V_NotWordHasChildrenIsLast => panic!("We should never leave off on !word?"),
-                    Instruction::W_IsWordNoChildrenNotLast => {
-                        println!("w - 1xpop");
-                        let n = self.control_stack.pop().unwrap();
-                        self.word_stack.drop_n(n.len()).unwrap();
-                        self.prio_addr_stack.pop().unwrap();
-                    },
-                    Instruction::X_IsWordNoChildrenIsLast => {
-                        println!("x - 2xpop");
 
-                        // Drop self
-                        let n = self.control_stack.pop().unwrap();
-                        self.word_stack.drop_n(n.len()).unwrap();
-                        self.prio_addr_stack.pop().unwrap();
+        // if !children:
+        //     pop one
+        //     if popped.last:
+        //         while peek.last: pop
+        //         pop one
+        //
+        // push + execute
+        //
+        if let Some(i) = self.control_stack.peek() {
+            // if !children:
+            if !i.has_children() {
 
-                        // Drop parent
-                        let n = self.control_stack.pop()?;
-                        self.word_stack.drop_n(n.len()).unwrap();
-                        if n.is_word() {
-                            self.prio_addr_stack.pop().unwrap();
-                        }
-                        if n.is_last() {
-                            loop {
-                                match self.control_stack.peek() {
-                                    Some(instr) if instr.is_last() => {
-                                        println!("  -> popslide!");
-                                        if instr.is_word() {
-                                            self.prio_addr_stack.pop().unwrap();
-                                        }
-                                        let n = self.control_stack.pop().unwrap();
-                                        self.word_stack.drop_n(n.len()).unwrap();
-                                    }
-                                    _ => break,
-                                }
-                            }
-                        }
-                    },
-                    Instruction::Y_IsWordHasChildrenNotLast => {
-                        println!("y - 0xpop");
-                    },
-                    Instruction::Z_IsWordHasChildrenIsLast => {
-                        println!("z - 0xpop");
-                    },
+                // pop one
+                let val = self.pop_cstack();
+
+                // if popped.last:
+                if val.is_last() {
+                    // while peek.last: pop
+                    while self.control_stack.peek()?.is_last() {
+                        self.pop_cstack();
+                    }
+
+                    // pop one
+                    self.pop_cstack();
                 }
-            },
-            _ => {},
+            }
         }
 
-
-
+        // push + execute
         loop {
             let cur_instr: Instr = (*self.program.get(self.program_ctr)?).into();
 
             // Push instr onto control stack
-            self.control_stack.push(cur_instr).unwrap();
+            self.control_stack.push(cur_instr)
+                .map_err(|_| { debug_assert!(false, "debug-only check failed: Control Stack Overflow"); })
+                .ok()?;
             self.program_ctr += 1;
 
+            // If it's a word, grab the priority byte
             if cur_instr.is_word() {
-                self.prio_addr_stack.push(self.program_ctr).unwrap();
+                self.prio_addr_stack.push(self.program_ctr)
+                    .map_err(|_| { debug_assert!(false, "debug-only check failed: Prio Stack Overflow"); })
+                    .ok()?;
                 self.program_ctr += 1;
             }
 
-            // Push contents onto word stack
+            // Push word contents onto word stack
             for _ in 0..cur_instr.len() {
-                self.word_stack.push(*self.program.get(self.program_ctr).unwrap()).unwrap();
+                self.word_stack.push(*self.program.get(self.program_ctr)?)
+                    .map_err(|_| { debug_assert!(false, "debug-only check failed: Word Stack Overflow"); })
+                    .ok()?;
                 self.program_ctr += 1;
             }
 
             if cur_instr.is_word() {
                 // TODO: at some point I will need to decode keycode to chars,
                 // user should provide scratch buffer
-                let word = core::str::from_utf8(self.word_stack.all()).unwrap();
+                let word = core::str::from_utf8(self.word_stack.all())
+                    .map_err(|_| { debug_assert!(false); })
+                    .ok()?;
                 println!(" --> {}", word);
                 break Some(word);
             }
@@ -282,26 +293,68 @@ impl T9Vm {
 pub mod test {
     use crate::{Instruction, Instr, T9Vm, Stack};
 
-    fn u(len: u8) -> u8 {
+    const fn u(len: u8) -> u8 {
         Instr::from_len_instr(len, Instruction::U_NotWordHasChildrenNotLast).0
     }
-    fn v(len: u8) -> u8 {
+    const fn v(len: u8) -> u8 {
         Instr::from_len_instr(len, Instruction::V_NotWordHasChildrenIsLast).0
     }
-    fn w(len: u8) -> u8 {
+    const fn w(len: u8) -> u8 {
         Instr::from_len_instr(len, Instruction::W_IsWordNoChildrenNotLast).0
     }
-    fn x(len: u8) -> u8 {
+    const fn x(len: u8) -> u8 {
         Instr::from_len_instr(len, Instruction::X_IsWordNoChildrenIsLast).0
     }
-    fn y(len: u8) -> u8 {
+    const fn y(len: u8) -> u8 {
         Instr::from_len_instr(len, Instruction::Y_IsWordHasChildrenNotLast).0
     }
-    fn z(len: u8) -> u8 {
+    const fn z(len: u8) -> u8 {
         Instr::from_len_instr(len, Instruction::Z_IsWordHasChildrenIsLast).0
     }
 
     const PRIO: u8 = 0;
+    const DEMO: &[u8] = &[
+        y(1), PRIO, b'a',
+        y(4), PRIO, b'a', b'r', b'o', b'n',
+        x(1), PRIO, b's',
+        y(1), PRIO, b'b',
+        x(2), PRIO, b'l', b'e',
+        u(2), b'p', b'p',
+        y(2), PRIO, b'l', b'e',
+        w(2), PRIO, b't', b's',
+        x(1), PRIO, b's',
+        z(4), PRIO, b'n', b'o', b't', b'e',
+        v(1), b'_',
+        z(1), PRIO, b'a',
+        z(1), PRIO, b'b',
+        y(1), PRIO, b'c',
+        x(1), PRIO, b'd',
+        z(1), PRIO, b'e',
+        x(1), PRIO, b'f',
+        x(1), PRIO, b's',
+        x(4), PRIO, b'b', b'i', b't', b'e',
+        ];
+/*
+
+    app
+       note
+           _
+            a
+             b
+              c
+               d
+              e
+               f
+    s
+*/
+
+// if !children:
+//     pop one
+//     if popped.last:
+//         while peek.last: pop
+//         pop one
+//
+// push + execute
 
     #[test]
     fn smoke() {
@@ -313,20 +366,7 @@ pub mod test {
         // 101 - X -  word, !children,  last
         // 110 - Y -  word,  children, !last
         // 111 - Z -  word,  children,  last
-        let program = vec![
-            y(1), PRIO, b'a',
-            y(4), PRIO, b'a', b'r', b'o', b'n',
-            x(1), PRIO, b's',
-            y(1), PRIO, b'b',
-            x(2), PRIO, b'l', b'e',
-            u(2), b'p', b'p',
-            y(2), PRIO, b'l', b'e',
-            w(2), PRIO, b't', b's',
-            x(1), PRIO, b's',
-            x(4), PRIO, b'n', b'o', b't', b'e',
-            x(1), PRIO, b's',
-            x(4), PRIO, b'b', b'i', b't', b'e',
-        ];
+        let program = DEMO.iter().copied().collect::<Vec<u8>>();
         let expected = [
             String::from("a"),
             String::from("aaron"),
@@ -337,6 +377,12 @@ pub mod test {
             String::from("applets"),
             String::from("apples"),
             String::from("appnote"),
+            String::from("appnote_a"),
+            String::from("appnote_ab"),
+            String::from("appnote_abc"),
+            String::from("appnote_abcd"),
+            String::from("appnote_abe"),
+            String::from("appnote_abef"),
             String::from("as"),
             String::from("bite"),
         ];
@@ -360,4 +406,75 @@ pub mod test {
             outs.as_slice(),
         );
     }
+
+    #[test]
+    fn sorted() {
+        assert!("a" < "as");
+        assert!("as" < "asd");
+        assert!("as" < "at");
+
+        // Len of proposed must be >= target
+        // proposed[..target.len()] == target
+    }
+
+    // #[test]
+    // fn submatch() {
+    //     // 000 - S - !word, !children, !last
+    //     // 001 - T - !word, !children,  last
+    //     // 010 - U - !word,  children, !last
+    //     // 011 - V - !word,  children,  last
+    //     // 100 - W -  word, !children, !last
+    //     // 101 - X -  word, !children,  last
+    //     // 110 - Y -  word,  children, !last
+    //     // 111 - Z -  word,  children,  last
+    //     let program = DEMO.iter().copied().collect::<Vec<u8>>();
+    //     let expected_a = [
+    //         String::from("a"),
+    //         String::from("aaron"),
+    //         String::from("aarons"),
+    //         String::from("ab"),
+    //         String::from("able"),
+    //         String::from("apple"),
+    //         String::from("applets"),
+    //         String::from("apples"),
+    //         String::from("appnote"),
+    //         String::from("as"),
+    //     ];
+    //     let expected_ap = [
+    //         String::from("apple"),
+    //         String::from("applets"),
+    //         String::from("apples"),
+    //         String::from("appnote"),
+    //     ];
+    //     let expected_app = [
+    //         String::from("apple"),
+    //         String::from("applets"),
+    //         String::from("apples"),
+    //     ];
+    //     let expected_appl = [
+    //         String::from("apple"),
+    //         String::from("applets"),
+    //         String::from("apples"),
+    //     ];
+    //     let expected_applz: [String; 0] = [];
+
+    //     let mut vm = T9Vm {
+    //         control_stack: Stack::<_>::default(),
+    //         word_stack: Stack::<_>::default(),
+    //         prio_addr_stack: Stack::<_>::default(),
+    //         program_ctr: 0,
+    //         program,
+    //     };
+
+    //     let mut outs = Vec::new();
+
+    //     while let Some(w) = vm.next_word() {
+    //         outs.push(w.to_string());
+    //     }
+
+    //     assert_eq!(
+    //         expected.as_slice(),
+    //         outs.as_slice(),
+    //     );
+    // }
 }
